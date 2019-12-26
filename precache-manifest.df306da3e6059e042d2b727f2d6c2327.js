d93bfdc7ca0b0c72ff3bb0c2149d6cbd010a1b32
17k695 <58382566+17k695@users.noreply.github.com>
2019-12-26 21:41:43 +0530

    Updates

---
+++ precache-manifest.df306da3e6059e042d2b727f2d6c2327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5d4899a74f49c7faa5d76103420aa9",
+    "url": "/Robofriend3/index.html"
+  },
+  {
+    "revision": "bc7905d8c486440723ee",
+    "url": "/Robofriend3/static/css/main.7109458d.chunk.css"
+  },
+  {
+    "revision": "0308d0b7a8d6f7c74827",
+    "url": "/Robofriend3/static/js/2.1a3d06f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robofriend3/static/js/2.1a3d06f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc7905d8c486440723ee",
+    "url": "/Robofriend3/static/js/main.7d7b740c.chunk.js"
+  },
+  {
+    "revision": "dc90fb85c4671c74e5bd",
+    "url": "/Robofriend3/static/js/runtime-main.718fc193.js"
+  }
+]);
